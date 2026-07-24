41313133b4d34007b286e7ce8cfdbf12e43599ba
youssefbelfeki <youssef.belfeki@gmail.com>
2026-07-24 11:52:25 +0100

    boilerplate project

---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { LoggerMiddleware } from './logger/logger.middleware';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(LoggerMiddleware)
+    .forRoutes('users');
+  }
+}
