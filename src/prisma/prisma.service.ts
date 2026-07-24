1feb02cbdb2ba056cb9d0c1df52a2d09fe8c525c
youssefbelfeki <youssef.belfeki@gmail.com>
2026-07-24 12:44:31 +0100

    feat: initialize Prisma ORM and implement basic product management module with DTOs and database integration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private static pool: pg.Pool;
+
+  constructor() {
+    const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+    PrismaService.pool = pool;
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    await PrismaService.pool.end();
+  }
+}
